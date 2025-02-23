9ff7fa93a488e273df45fead1b52e04c936711a6
BryanAlzate57 <bryan.alzate@correounivalle.edu.co>
2025-02-22 20:36:29 -0500

    Organizacion de la pagina

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -6,10 +6,11 @@ const Footer = () => {
   <footer>
     <ul className="ulinks">
       <li><Link to="/riñon/calculos-renales" className="footer-link">Cálculos Renales</Link></li>
-      <li><Link to="/riñon/enfermedad-2" className="footer-link">Enfermedad 2</Link></li>
-      <li><Link to="/riñon/enfermedad-3" className="footer-link">Enfermedad 3</Link></li>
-      <li><Link to="/riñon/enfermedad-4" className="footer-link">Enfermedad 4</Link></li>
+      <li><Link to="/riñon/Cancer-Riñon" className="footer-link">Cancer Riñon</Link></li>
+      <li><Link to="/riñon/enfermedad-renal-cronica" className="footer-link">Enfermedad Renal Cronica</Link></li>
+      <li><Link to="/riñon/glomerulonefritis" className="footer-link">Glomerulonefritis</Link></li>
     </ul>
+    <p className="copyright-text">Copyright 2025</p>
   </footer>
   );
 };

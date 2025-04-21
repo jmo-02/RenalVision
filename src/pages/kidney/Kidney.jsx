79b16d7e12bfd038dcc80b05409a8f03e52260af
BryanAlzate57 <bryan.alzate@correounivalle.edu.co>
2025-04-21 02:10:16 -0500

    Cambios realizados en la rama master en enfermedades

--- src/pages/kidney/Kidney.jsx
+++ src/pages/kidney/Kidney.jsx
@@ -13,6 +13,11 @@ const Kidney = () => {
     navigate('/riñon/glomerulonefritis');
   }, [navigate]);
 
+  const handleClickCancer = useCallback(() => {
+      navigate("/riñon/cancer-de-riñon", {
+      });
+      }, [navigate]);
+
   return (
     <div className="kidney-container">
       <h2 className="kidney-title">
@@ -28,6 +33,12 @@ const Kidney = () => {
           <img src="/images/glomerulonefritis.png" alt="Ícono de Glomerulonefritis" />
           <button onClick={handleGlomerulonefritis} className="kidney-button">
             Glomerulonefritis
+          </button>
+        </div>
+
+        <div className="button-team">
+          <button onClick={handleClickCancer} className="boton2">
+            Cáncer de Riñón
           </button>
         </div>
       </div>
